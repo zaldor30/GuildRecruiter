e086486a1649da1769e1671c1af5d6753dfa3335
Zalron <68781399+zaldor30@users.noreply.github.com>
2024-08-13 12:23:35 -0400

    3.0.26

---
+++ locales/enUS.lua
@@ -0,0 +1,288 @@
+-- Localization file for English/United States
+local L = LibStub("AceLocale-3.0"):NewLocale("GuildRecruiter", "enUS", true)
+
+-- * General
+L["TITLE"] = "Guild Recruiter"
+L["INVITE"] = 'Invite'
+L["SCAN"] = 'Scan'
+L["ABOUT"] = 'About'
+L["CLOSE"] = 'Close'
+L["CANCEL"] = 'Cancel'
+L["SAVE"] = 'Save'
+L["YES"] = 'Yes'
+L["NO"] = 'No'
+L["OK"] = 'OK'
+L["ENABLE"] = 'Enable'
+L["DISABLE"] = 'Disable'
+L["REMOVE"] = 'Remove'
+L["NEW"] = 'New'
+L["DELETE"] = 'Delete'
+
+-- * WoW System Message Translations
+--! MUST BE IN LOWERCASE!
+L['PLAYER_NOT_ONLINE'] = "is not online"
+L['PLAYER_NOT_PLAYING'] = "is currently playing"
+L['NO_PLAYER_NAMED'] = "no player named"
+L['PLAYER_NOT_FOUND'] = "no player named" -- Duplicate removed
+L['PLAYER_IN_GUILD'] = "has already been invited to a guild"
+L['PLAYER_ALREADY_IN_GUILD'] = "is already in a guild"
+L['PLAYER_JOINED_GUILD'] = "has joined the guild"
+L['PLAYER_DECLINED_INVITE'] = "declines your guild invitation"
+
+-- * Donation Message
+L['DONATION_MESSAGE'] = [[
+I hope you find this addon useful. I have put a lot of time and effort into
+making this addon. If you would like to donate, please use the link below.
+Thank you for your support!]]
+
+-- * GR Basic Command Messages
+L['INVITE_MESSAGES_MENU'] = 'Invite with Messages'
+L['INVITE_NO_MESSAGES_MENU'] = 'Invite without Messages'
+L['DELETE_CONFIRMATION'] = 'Are you sure you want to delete this record?'
+L['ABOUT_TOOLTIP'] = 'See What\'s New and support info.'
+L['PLEASE_WAIT'] = 'Please wait'
+L['ERROR_SCAN_WAIT'] = ' seconds before scanning again.'
+
+-- * Version 3.0 Changes
+
+-- * GR Icon Bar
+L['LOCK'] = 'Lock'
+L['LOCK_TOOLTIP'] = 'Lock or unlock the window from repositioning.'
+L['SETTINGS'] = 'Settings'
+L['SETTINGS_TOOLTIP'] = 'Open the Guild Recruiter settings window.'
+L['SYNC'] = 'Sync'
+L['SYNC_TOOLTIP'] = 'Manual sync with guild members.'
+L['ANALYTICS'] = 'Analytics'
+L['ANALYTICS_TOOLTIP'] = 'View the analytics window.'
+L['BLACKLIST'] = 'Black List'
+L['BLACKLIST_TOOLTIP'] = 'Add a player to the blacklist.'
+L['BLACK_LIST_REASON_INPUT'] = 'Why do you want to black list?'
+L['BL_NAME_NOT_ADDED'] = 'Black list name not added.'
+L['ADDED_TO_BLACK_LIST'] = 'was added to the black list with %s as a reason.'
+L['FILTER_EDITOR'] = 'Filter Editor'
+L['FILTER_EDITOR_TOOLTIP'] = 'Open the filter editor window.'
+L['COMPACT_MODE'] = 'Compact Mode'
+L['COMPACT_MODE_TOOLTIP'] = 'Toggle compact scanner compact mode.'
+L['RESET_FILTER'] = 'Reset Filter'
+L['RESET_FILTER_TOOLTIP'] = 'Reset the filter to the restart scan.'
+L['FILTERS'] = 'Filters'
+
+-- * Icon Menu and Slash Commands
+L["HELP"] = 'help'
+L["CONFIG"] = 'config'
+L["RELOAD"] = 'reload'
+L["RECRUITER"] = 'recruiter'
+L['HOME_BUTTON'] = 'Home'
+
+-- * GR General Messages
+L['IS_ENABLED'] = 'is enabled.'
+
+-- * Default Values
+L['DEFAULT_GUILD_WELCOME'] = 'Welcome PLAYERNAME to GUILDNAME!'
+L['GUILDLINK'] = 'GUILDLINK'
+L['GUILD_LINK_NOT_FOUND'] = 'No Guild Link'
+L['GUILDNAME'] = 'GUILDNAME'
+L['NO_GUILD_NAME'] = 'No Guild Name'
+L['PLAYERNAME'] = 'PLAYERNAME'
+L['NO_PLAYER_NAME'] = 'player'
+L['BLACK_LIST'] = 'Black List'
+L['OK_INVITE'] = 'Do you want to invite anyway?'
+L['NO_INVITE_MESSAGE'] = 'No message selected. Please select a message in the home screen.'
+L['INVITE_IN_GUILD'] = 'is already in the guild'
+L['IS_ON_SPAM_LIST'] = 'is on the Anti-Spam list'
+L['GUILD_INVITE_SENT'] = 'Guild invite sent to'
+L['INVITE_MESSAGE_SENT'] = 'Invite message sent to'
+L['INVITE_ALREADY_SENT'] = 'has already been invited'
+L['NO_GUILD_LINK'] = 'No guild link found. Sync with GM or log into a GM character.'
+L['SELECT_MESSAGE'] = 'Select a message from the list or create one in settings.'
+
+--* GR Filter Messages
+L['FILTERS'] = 'Filters'
+L['DELETE_FILTER'] = 'Delete Filter'
+L['DELETE_FILTER_CONFIRM'] = 'Are you sure you want to delete this filter?'
+L['FILTER_DESC'] = 'Description of the filter'
+L['WHO_COMMAND'] = 'Who Command'
+L['CLASSES'] = 'Classes'
+L['RACES'] = 'Races'
+
+-- * GR Scanner Messages
+L['BL'] = 'BL'
+L['BLACK_LISTED'] = 'Black listed'
+L['IS_ON_BLACK_LIST'] = 'is on the Black List.'
+L['BLACK_LIST_CONFIRM'] = 'Are you sure you want to add this player to the black list?'
+L['ANTI_SPAM'] = 'Anti-Spam'
+L['READY_INVITE'] = 'Ready to invite'
+L['BL_ADD_PLAYER'] = 'Add Player to black list'
+L['SKIP'] = 'Skip'
+L['SKIP_DESC'] = 'Skip the current player and move to the next player.'
+L['WHO_RESULTS'] = 'Who Results'
+L['NEXT_FILTER'] = 'Next Query'
+L['FILTER_PROGRESS'] = 'Filter Progress'
+L['RESETTING_FILTERS'] = 'Resetting filters on next scan.'
+L['NUMBER_PLAYERS_FOUND'] = 'Number of players found'
+L['INVITE_BUTTON_TOOLTIP'] = 'Invite player to the guild.'
+L['INVITE_BUTTON_BODY_TOOLTIP'] = [[Only unchecked players will be invited.
+Checked players are for black list and skip.]]
+L['BL_BUTTON_TOOLTIP'] = 'Add player to the blacklist.'
+L['BL_BUTTON_BODY_TOOLTIP'] = [[Add the player to the blacklist and skip to the next player.
+Can only invite if no players are checked.]]
+L['SKIP_BUTTON_TOOLTIP'] = 'Skip player and move to the next player.'
+L['SKIP_BUTTON_BODY_TOOLTIP'] = [[Skip the current player and move to the next player.
+This will add the player to already invited and will try to
+invite the player after the anti-spam expires.
+Can only invite if no players are checked.]]
+
+-- * Analytics
+L['TOTAL_SCANNED'] = 'Players Scanned'
+L['TOTAL_INVITED'] = 'Players Invited'
+L['INVITES_PENDING'] = 'Invites Pending'
+L['TOTAL_DECLINED'] = 'Invites Declined'
+L['TOTAL_ACCEPTED'] = 'Invites Accepted'
+L['TOTAL_BLACKLISTED'] = 'Black Listed'
+L['TOTAL_ANTI_SPAM'] = 'Players on Anti-Spam'
+L['SESSION_STATS'] = 'Session Stats'
+
+-- * GR Core Messages
+L['FIRST_TIME_INFO'] = [[
+Welcome to Guild Recruiter!
+You can access by right clicking on the minimap icon
+or by typing /gr config. Left click on the minimap
+icon to open the recruitment window.
+If you have any issues, click on the About menu
+option to get our Discord link.]]
+L['NEW_VERSION_INFO'] = [[
+Guild Recruiter has been updated!
+Please check the "What's New?" for what has changed.]]
+L['BETA_INFORMATION'] = [[This is a VER version of Guild Recruiter.
+Please report any issues on our Discord server.]]
+
+-- * GR Error Messages
+L['NO GUILD'] = "You are not in a guild."
+L['NOT_LOADED'] = 'Guild Recruiter will not load.'
+L['CANNOT_INVITE'] = 'You do not have permission to invite to the guild.'
+
+-- * GR Slash Commands
+L['SLASH_COMMANDS'] = [[
+Guild Recruiter Slash Commands:
+/rl will reload the WoW UI (like /reload).
+/gr help - Displays this help message.
+/gr config - Opens the configuration window.
+/gr blacklist <player name> - Will add the player to the blacklist.]]
+
+-- * GR Minimap Icon Tooltip
+-- Keep %AntiSpam and %BlackList in the tooltip.
+L['MINIMAP_TOOLTIP'] = [[
+Left Click: Open Guild Recruiter
+Shift+Left Click: Open Scanner
+Right Click: Open Config
+%AntiSpam in invited list.
+%BlackList in blacklisted list.]]
+
+-- * GR Home Screen
+L['MESSAGE_ONLY'] = 'Message ONLY'
+L['GUILD_INVITE_ONLY'] = 'Guild Invite ONLY'
+L['GUILD_INVITE_AND_MESSAGE'] = 'Guild Invite and Message'
+L['MESSAGE_ONLY_IF_INVITE_DECLINED'] = 'Message Only if Invitation is declined'
+L['CLASS_FILTER'] = 'Default Class Filter'
+L['RACE_FILTER'] = 'Default Race Filter'
+L['INVITE_FORMAT'] = 'Recruit Invite Format:'
+L['MIN_LVL'] = 'Min Level:'
+L['MAX_LVL'] = 'Max Level:'
+L['MESSAGE_LIST'] = 'Invite Messages'
+L['INVITE_MESSAGE_ONLY'] = 'Send Invite Message Only'
+
+-- * GR Error Message
+L['INVALID_LEVEL'] = 'You must enter a number between 1 and'
+L['MIN_LVL_HIGHER_ERROR'] = 'The minimum level must be higher than the maximum level.'
+L['MAX_LVL_LOWER_ERROR'] = 'The maximum level must be lower than the minimum level.'
+
+-- * GR Config Window Messages
+L['General Settings'] = true
+L['MESSAGE_REPLACEMENT_INSTRUCTIONS'] = [[
+GUILDLINK - Will create a clickable link to your guild.
+GUILDNAME - Will display your guild name.
+PLAYERNAME - Will display the invited player's name.]]
+L['INVITE_DESC'] = 'Description of the invite message:'
+L['INVITE_DESC_TOOLTIP'] = 'A description of the invite message.'
+L['INVITE_ACTIVE_MESSAGE'] = 'Invite Messages:'
+L['INVITE_ACTIVE_MESSAGE_DESC'] = 'The messages that will be sent to potential recruits. Note: You might need to /rl after a sync to see changes.'
+L['NEW_MESSAGE_DESC'] = 'Add a description of the message to the invite list.'
+
+-- * GR Config Window Tooltips
+L['GEN_TOOLTIPS'] = 'Show all tooltips'
+L['GEN_TOOLTIP_DESC'] = 'Show all tooltips in the Guild Recruiter addon'
+L['GEN_MINIMAP'] = 'Show Minimap Icon'
+L['GEN_MINIMAP_DESC'] = 'Show the Guild Recruiter minimap icon.'
+L['GEN_CONTEXT'] = 'Enable inviting from chat'
+L['GEN_CONTEXT_DESC'] = 'Show the Guild Recruiter context menu when right-clicking a name in chat.'
+L['GEN_WHATS_NEW'] = 'Show What\'s New?'
+L['AUTO_SYNC'] = 'Auto sync at login'
+L['AUTO_SYNC_DESC'] = 'Automatically sync with guild members when logging in.'
+L['SHOW_WHISPERS'] = 'Show whispers |cFF00FF00Do a /rl after changing|r'
+L['SHOW_WHISPERS_DESC'] = 'Show the message you send to players when inviting.'
+L['SCAN_WAIT_TIME'] = 'Scan delay in seconds'
+L['SCAN_WAIT_TIME_DESC'] = 'The time in seconds to wait before scanning for players (2 to 10 seconds).'
+L['GEN_WHATS_NEW_DESC'] = 'Show the What\'s New? window when Guild Recruiter is updated.'
+L['GEN_ADDON_MESSAGES'] = 'Show System Messages'
+L['GEN_ADDON_MESSAGES_DESC'] = 'Show system messages from Guild Recruiter.'
+L['KEYBINDING_HEADER'] = 'Keybindings'
+L['KEYBINDING_INVITE'] = 'Invite Keybinding'
+L['KEYBINDING_INVITE_DESC'] = 'Keybinding to invite a player to the guild.'
+L['KEYBINDING_SCAN'] = 'Scan Keybinding'
+L['KEYBINDING_SCAN_DESC'] = 'Keybinding to scan for players looking for a guild.'
+L['KEY_BINDING_NOTE'] = 'Note: Keybinds will not affect WoW keybindings.'
+L['GEN_ACCOUNT_WIDE'] = 'indicates effects all guild-wide characters'
+
+-- * GR GM Settings Window
+L['GM_SETTINGS'] = 'GM Settings'
+L['GM_SETTINGS_DESC'] = 'Note: Guild Masters have access to these settings on all their characters.'
+L['MAX_CHARS'] = '(<sub> characters per message)'
+L['LENGTH_INFO'] = 'Assumes 12 characters when using PLAYERNAME'
+L['MESSAGE_LENGTH'] = 'Message Length'
+
+-- * GR GM Invite Settings Window
+L['GM_INVITE'] = 'GM Messages'
+L['ENABLED_NOTE'] = 'Note: Disabled items are controlled by GM.'
+L['OVERRIDE_GM_SETTINGS'] = 'Override GM Settings'
+L['OVERRIDE_GM_SETTINGS_DESC'] = 'Override the GM settings for this character.'
+
+-- * GR Invite Settings Window
+L['INVITE_SETTINGS'] = 'Invite Settings'
+L['ENABLE_ANTI_SPAM'] = 'Enable Anti-Spam'
+L['ENABLE_ANTI_SPAM_DESC'] = 'Enable the Anti-Spam feature to prevent spamming players.'
+L['ANTI_SPAM_DAYS'] = 'Re-invite Delay'
+L['ANTI_SPAM_DAYS_DESC'] = 'Number of days before inviting a player again.'
+L['GUILD_WELCOME_MSG'] = 'Guild Welcome Message'
+L['GUILD_WELCOME_MSG_DESC'] = 'The message to send to guild chat when a new player joins.'
+L['WHISPER_WELCOME_MSG'] = 'Whisper Welcome Message'
+L['WHISPER_WELCOME_MSG_DESC'] = 'Whispered message sent to a player when they join the guild.'
+
+-- * GR Invite Messages Window
+L['INVITE_MESSAGES'] = 'Invite Messages'
+
+-- * BlackList Settings Window
+L['BLACK_LIST'] = 'Black List'
+L['BLACK_LIST_REMOVE'] = 'Remove Selected Black List Entries'
+L['ADD_TO_BLACK_LIST'] = 'Add player to black list.'
+
+-- * Invalid Settings Window
+L['INVALID_ZONE'] = 'Invalid Zones'
+L['ZONE_ID'] = 'Zone ID (Numeric ID)'
+L['ZONE_NAME'] = 'Name of the Zone:'
+L['ZONE_TYPE'] = 'Type of Zone:'
+L['ZONE_ID_DESC'] = [[
+The zone ID for the invalid zone.
+List of instances:
+https://wowpedia.fandom.com/wiki/InstanceID
+Best World Zone IDs I can Find:
+https://wowpedia.fandom.com/wiki/UiMapID
+If you find a zone that should be added, please let me know.]]
+L['ZONE_NOTE'] = 'Zones with |cFF00FF00*|r are the only editable zones.'
+L['ZONE_LIST_NAME'] = 'The following zones will be ignored by the scanner:'
+
+-- * About
+L['ABOUT_LINE'] = 'Thank you for using Guild Recruiter, I hope you find this addon useful!'
+L['ABOUT_DOC_LINKS'] = 'Documentation and Links'
+L['GITHUB_LINK'] = 'GitHub (Support documentation)'
+L['ABOUT_DISCORD_LINK'] = 'Discord Link'
+L['SUPPORT_LINKS'] = 'Support Guild Recruiter Links'
---
+++ shared/GR_Code.lua
@@ -0,0 +1,202 @@
+local _, ns = ... -- Namespace (myaddon, namespace)
+local L = LibStub("AceLocale-3.0"):GetLocale('GuildRecruiter')
+
+local LibDeflate = LibStub:GetLibrary("LibDeflate")
+local aceSerializer = LibStub:GetLibrary("AceSerializer-3.0")
+
+ns.code = {}
+local code = ns.code
+function code:Init()
+    self.fPlayerName = nil
+end
+-- *Console Text Output Routines
+function code:cText(color, text)
+    if text == '' then return end
+
+    color = color == '' and 'FFFFFFFF' or color
+    return '|c'..color..text..'|r'
+end
+function code:cPlayer(name, class, color) -- Colorize player names
+    if name == '' or ((not class or class == '') and (not color or color == '')) or not name then return end
+    local c = (not class or class == '') and color or select(4, GetClassColor(class))
+
+    if c then return code:cText(c, name)
+    else return end
+end
+function code:consolePrint(msg, color, noPrefix) -- Console print routine
+    if msg == '' or not msg then return end
+
+    local prefix = not noPrefix and self:cText(GRColor, 'GR: ') or ''
+    color = strlen(color) == 6 and 'FF'..color or color
+    DEFAULT_CHAT_FRAME:AddMessage(prefix..code:cText(color or 'FFFFFFFF', msg))
+end
+function code:cOut(msg, color, noPrefix) -- Console print routine
+    if msg == '' or not msg then return end
+
+    --!Check to show console messages
+    code:consolePrint(msg, (color or '97FFFFFF'), noPrefix)
+end
+function code:dOut(msg, color, noPrefix) -- Debug print routine
+    if msg == '' or not GR.debug then return end
+    code:consolePrint(msg, (color or 'FFD845D8'), noPrefix)
+end
+function code:fOut(msg, color, noPrefix) -- Force console print routine)
+    if msg == '' then return
+    else code:consolePrint(msg, (color or '97FFFFFF'), noPrefix) end
+end
+
+-- * Data Compression Routines
+function code:compressData(data, encode)
+    if not data then return end
+
+    local serializedData = aceSerializer:Serialize(data)
+    local compressedData = LibDeflate:CompressDeflate(serializedData)
+    return (encode and LibDeflate:EncodeForWoWAddonChannel(compressedData) or compressedData)
+end
+function code:decompressData(data, decode)
+    if not data or data == '' or type(data) ~= 'string' then return false, nil end
+
+    data = (decode and LibDeflate:DecodeForWoWAddonChannel(data) or data)
+    local decompressedData = LibDeflate:DecompressDeflate(data)
+    if decompressedData then return aceSerializer:Deserialize(decompressedData)
+    else return false, nil end -- Decompression failed
+end
+
+-- * Tables and Data Sorting Routines
+function code:saveTables(whichOne)
+    if whichOne == 'BLACK_LIST' then ns.g.blackList = ns.code:compressData(ns.tblBlackList)
+    elseif whichOne == 'ANTI_SPAM_LIST' then ns.g.antiSpamList = ns.code:compressData(ns.tblAntiSpamList)
+    else
+        ns.g.blackList = ns.code:compressData(ns.tblBlackList) or ''
+        ns.g.antiSpamList = ns.code:compressData(ns.tblAntiSpamList) or ''
+        if ns.guildSession then ns.gAnalytics.session = ns.code:compressData(ns.guildSession) end
+    end
+end
+function code:sortTableByField(tbl, sortField, reverse, showit)
+    if not tbl or not sortField then return end
+
+    local keyArray = {}
+    for key, rec in pairs(tbl) do
+        if type(key) == 'string' then
+            rec.key = key
+            table.insert(keyArray, rec)
+        end
+    end
+
+    reverse = reverse or false
+    local sortFunc = function(a, b)
+        if a[sortField] and b[sortField] then
+            if reverse then return a[sortField] > b[sortField]
+            else return a[sortField] < b[sortField] end
+        end
+    end
+
+    table.sort(keyArray, sortFunc)
+    return keyArray
+end
+
+-- *Tooltip Routine
+function code:createTooltip(text, body, force, frame)
+    if not force and not ns.gSettings.showToolTips then return end
+    local uiScale, x, y = UIParent:GetEffectiveScale(), GetCursorPosition()
+    if frame then uiScale, x, y = 0, 0, 0 end
+    CreateFrame("GameTooltip", nil, nil, "GameTooltipTemplate")
+    GameTooltip_SetDefaultAnchor(GameTooltip, UIParent)
+    GameTooltip:SetOwner(UIParent, "ANCHOR_CURSOR") -- Attaches the tooltip to cursor
+    GameTooltip:SetPoint("BOTTOMLEFT", (frame or nil), "BOTTOMLEFT", (uiScale ~= 0 and (x / uiScale) or 0),  (uiScale ~= 0  and (y / uiScale) or 0))
+    GameTooltip:SetText(text)
+    if body then GameTooltip:AddLine(body,1,1,1) end
+    GameTooltip:Show()
+end
+
+-- *Keyword Replacement Routines
+function code:capitalKeyWord(input)
+    if not input or input == '' then return end
+
+    local keys = { L['GUILDLINK'], L['GUILDNAME'], L['PLAYERNAME'] }
+
+    for i=1,#keys do
+        local startPos, endPos = (strupper(input)):find(keys[i])
+
+        if startPos then
+            local prefix = string.sub(input, 1, startPos - 1)
+            local suffix = string.sub(input, endPos + 1)
+            input = prefix..strupper(keys[i])..suffix
+        end
+    end
+
+    return input
+end
+function code:variableReplacement(msg, playerName, removeGT)
+    local gi = ns.guildInfo
+    if not gi or not msg or msg == '' then return end
+
+    playerName = (playerName and playerName ~= '') and strsplit('-', playerName) or ''
+
+    msg = code:capitalKeyWord(msg)
+    if not msg then return end
+
+    local gLink, gName = gi.guildLink or nil, gi.guildName or nil
+
+    msg = msg:gsub(L['GUILDLINK'], (gLink or L['No Guild Link']))
+    msg = msg:gsub(L['GUILDNAME'], (gName and (removeGT and gName or '<'..gName..'>') or L['No Guild Name']))
+    msg = msg:gsub(L['PLAYERNAME'], (playerName or 'player'))
+
+    return msg
+end
+
+-- *Frame Routines
+function code:createPadding(frame, rWidth)
+    local widget = LibStub("AceGUI-3.0"):Create('Label')
+    if rWidth <=2 then widget:SetRelativeWidth(rWidth)
+    else widget:SetWidth(rWidth) end
+    frame:AddChild(widget)
+end
+function code:wordWrap(inputString, maxLineLength)
+    local lines = {}
+    local currentLine = ""
+
+    maxLineLength = maxLineLength or 50
+    for word in inputString:gmatch("%S+") do
+        if #currentLine + #word <= maxLineLength then
+            currentLine = currentLine .. " " .. word
+        else
+            table.insert(lines, currentLine)
+            currentLine = word
+        end
+    end
+
+    table.insert(lines, currentLine)
+    return table.concat(lines, "\n")
+end
+function code:Confirmation(msg, func)
+    StaticPopupDialogs["MY_YES_NO_DIALOG"] = {
+        text = msg,
+        button1 = "Yes",
+        button2 = "No",
+        OnAccept = func,
+        timeout = 0,
+        whileDead = true,
+        hideOnEscape = false,
+    }
+    StaticPopup_Show("MY_YES_NO_DIALOG")
+end
+
+--* Miscellaneous Routines
+function code:inc(data, count) return (data or 0) + (count or 1) end
+function code:ConvertDateTime(val, showHours)
+    local d = date("*t", val)
+    return (d and showHours) and string.format("%02d/%02d/%04d %02d:%02d", d.month, d.day, d.year, d.hour, d.minute) or (string.format("%02d/%02d/%04d", d.month, d.day, d.year) or nil)
+end
+function code:isInMyGuild(name)
+    local realmName = name:find('-') and name or name..'-'..GetRealmName()
+    local noRealmName = name:gsub('*-', '')
+
+    local totalMembers = GetNumGuildMembers()
+    for i = 1, totalMembers do
+        local gName = GetGuildRosterInfo(i)
+        if strlower(gName) == strlower(noRealmName) then return true
+        elseif strlower(gName) == strlower(realmName) then return true end
+    end
+    return false
+end
