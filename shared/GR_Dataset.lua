7b919939e1fdee299542a6e265a34aff05f1c954
Zalron <68781399+zaldor30@users.noreply.github.com>
2024-08-25 11:58:28 -0400

    Update GR_Dataset.lua

--- shared/GR_Dataset.lua
+++ shared/GR_Dataset.lua
@@ -19,7 +19,7 @@ function ds:WhatsNew() -- What's new in the current version
                     Discord: https://discord.gg/ZtS6Q2sKRH
                 (or click on the icon in the top left corner)|r
 
-    |CFFFFFF00v3.0.36 Notes|r
+    |CFFFFFF00v3.0.37 Notes|r
         - Added option to keep addon running and ignore certain ways to close it.
     |CFFFFFF00v3.0.36 Notes|r  -- Joined forces with FGI to bring recruiting to the next level.
         - Changed the 6 months to 180 days.
