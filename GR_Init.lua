afc60c16e2cb1d22c215ff3c7047c829bd20174e
Zalron <68781399+zaldor30@users.noreply.github.com>
2024-08-25 11:57:54 -0400

    Update GR_Init.lua

--- GR_Init.lua
+++ GR_Init.lua
@@ -5,7 +5,7 @@ ICON_PATH = 'Interface\\AddOns\\GuildRecruiter\\Images\\'
 
 GR = LibStub('AceAddon-3.0'):NewAddon('GuildRecruiter', 'AceConsole-3.0', 'AceEvent-3.0', 'AceHook-3.0', 'AceComm-3.0')
 GR.debug = false
-GR.isTest = false
+GR.isTest = true
 GR.testLevel = 'Beta'
 GR.author  = C_AddOns.GetAddOnMetadata('GuildRecruiter', 'Author')
 GR.version = C_AddOns.GetAddOnMetadata('GuildRecruiter', 'Version')
