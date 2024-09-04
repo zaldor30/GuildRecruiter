77506333f4a298f95fb445621be59976f32f7687
Zalron <68781399+zaldor30@users.noreply.github.com>
2024-09-04 01:07:09 -0400

    Update GR_Init.lua

--- GR_Init.lua
+++ GR_Init.lua
@@ -8,8 +8,7 @@ GR = LibStub('AceAddon-3.0'):NewAddon('GuildRecruiter', 'AceConsole-3.0', 'AceEv
 GR.author  = C_AddOns.GetAddOnMetadata('GuildRecruiter', 'Author')
 GR.version = C_AddOns.GetAddOnMetadata('GuildRecruiter', 'Version')
 GR.dbVersion = '3.2'
-GR.isTesting = true --! PAY ATTENTION TO THIS VARIABLE
-GR.isPreRelease = false
+GR.isPreRelease = true
 GR.preReleaseType = 'Beta'
 GR.versionOut = '(v'..GR.version..(GR.isPreRelease and ' '..GR.preReleaseType or '')..')'
 
