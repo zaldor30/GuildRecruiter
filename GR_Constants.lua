4fd83e76182fc346c2708cd6e5953f75aaa99346
Zalron <68781399+zaldor30@users.noreply.github.com>
2024-11-30 12:02:16 -0500

    build

--- GR_Constants.lua
+++ GR_Constants.lua
@@ -9,7 +9,7 @@ GR.debug = false
 GR.isTesting = false -- Used for testing purposes (invite)
 GR.enableFilter = false
 GR.isPreRelease = true
-GR.preReleaseType = 'Alpha'
+GR.preReleaseType = 'Beta'
 GR.versionOut = '(v'..GR.version..(GR.isPreRelease and ' '..GR.preReleaseType or '')..')'
 
 C_ChatInfo.RegisterAddonMessagePrefix(GR.commPrefix)
