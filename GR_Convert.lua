39e8ce3dbafe5cf2a085bf46a0c525da24b1016d
Zalron <68781399+zaldor30@users.noreply.github.com>
2024-12-02 21:50:49 -0500

    Fixes

--- GR_Convert.lua
+++ GR_Convert.lua
@@ -33,30 +33,36 @@ function convert:ConvertFrom3to4()
     local oldData = self.oldData
 
     local db = DB:New(GR.db)
-    if not db or not db.global.dbVersion or type(db.global.dbVersion) == 'number' then return false end
+    if db then db.global.dbVer = nil end
+    if (db and db.global.dbVersion and type(db.global.dbVersion) == 'number') and db.global.dbVersion >= 4 then return false end
 
     ns.blacklist, ns.tblAntiSpamList = {}, {}
     for k, v in pairs(db.global) do
         if type(k) == 'number' then
             oldData[k] = v
-            local success, tbl = ns.code:decompressData(v.antiSpamList)
-            oldData[k].antiSpamList = ''
-            if success and tbl then
-                for k1, v1 in pairs(tbl) do
-                    local key = strlower(k1:match('-') and k1 or k1..'-'..GetRealmName())
-                    ns.tblAntiSpamList[key] = {
-                        time = v1.date,
-                        name = v1.name,
-                    }
+
+            if v.antiSpamList then
+                local success, tbl = ns.code:decompressData(v.antiSpamList)
+                oldData[k].antiSpamList = ''
+                if success and tbl then
+                    for k1, v1 in pairs(tbl) do
+                        local key = strlower(k1:match('-') and k1 or k1..'-'..GetRealmName())
+                        ns.tblAntiSpamList[key] = {
+                            time = v1.date,
+                            name = v1.name,
+                        }
+                    end
                 end
             end
 
-            success, tbl = ns.code:decompressData(v.blackList)
-            oldData[k].blackList = ''
-            if success and tbl then
-                for k1, v1 in pairs(tbl) do
-                    local key = strlower(k1:match('-') and k1 or k1..'-'..GetRealmName())
-                    ns.blacklist[key] = v1
+            if v.blackList then
+                local success, tbl = ns.code:decompressData(v.blackList)
+                oldData[k].blackList = ''
+                if success and tbl then
+                    for k1, v1 in pairs(tbl) do
+                        local key = strlower(k1:match('-') and k1 or k1..'-'..GetRealmName())
+                        ns.blacklist[key] = v1
+                    end
                 end
             end
         end
@@ -75,6 +81,7 @@ function convert:ConvertFrom3to4()
         ns.guild = db.global[k]
         ns.code:saveTables()
     end
+    db.global.dbVersion = 4
 
     return true, '3to4'
 end
