eb68755b545deb7b9d6baf5e0d140352207b01b3
Zalron <68781399+zaldor30@users.noreply.github.com>
2024-12-04 01:17:33 -0500

    Update GR_Core.lua
    Guild Link Fix

--- GR_Core.lua
+++ GR_Core.lua
@@ -97,10 +97,10 @@ function core:Init()
         guildLeaderToon = nil,
         gmActive = false,
         analytics = {},
-        blackList = "",
+        blackList = {},
         filterList = {},
         messageList = {},
-        antiSpamList = "",
+        antiSpamList = {},
         blackListRemoved = {},
         lastSync = {}, -- Who, Date
     }
@@ -138,6 +138,15 @@ function core:StartGuildRecruiter(clubID)
 
     ns.base:SetShown(true, true)
 
+    local function checkForFGI(retry)
+        retry = retry + 1
+        local isFGILoaded = C_AddOns.IsAddOnLoaded('FastGuildInvite')
+        if isFGILoaded then ns.code:fOut(L['FGI_LOADED'], ns.COLOR_ERROR) return
+        elseif retry > 5 then return
+        elseif retry <= 5 then C_Timer.After(1, function() checkForFGI(retry + 1) end) end
+    end
+    checkForFGI(0)
+
     ns.code:fOut(L['TITLE']..' '..GR.versionOut..' '..L['ENABLED'], ns.COLOR_DEFAULT, true)
     if GR.isPreRelease then
         ns.code:fOut(L['BETA_INFORMATION']:gsub('VER', ns.code:cText('FFFF0000', strlower(GR.preReleaseType))), 'FFFFFF00', true)
@@ -146,9 +155,24 @@ function core:StartGuildRecruiter(clubID)
     self.isEnabled = true
 end
 function core:StartDatabase(clubID)
-    local continueConvert, whichOne = ns.convert:ConvertFrom3to4() -- Convert the database from version 3 to version 4
-
+    local wasReset = false
     local db = DB:New(GR.db, self.fileStructure) -- Initialize the database
+
+    local function resetDatabase()
+        db.global = db.global and table.wipe(db.global) or {} -- Reset the global database
+        db:ResetProfile() -- Reset current profile
+
+        -- Optionally, delete all other profiles by iterating through them
+        for profileName in pairs(db.profiles) do
+            db.profiles[profileName] = nil  -- Delete profile data
+        end
+
+        wasReset = true
+        db.global.dbVer = GR.dbVersion -- Set the database version
+        ns.code:fOut(L['DATABASE_RESET'], ns.COLOR_ERROR)
+    end
+
+    if not db.global.dbVer or (db.global.dbVer < 4 and db.global.dbVer ~= GR.dbVersion) then resetDatabase() end -- Reset the database if the version is different
 
     if not db.global[clubID] then
         db.global[clubID] = self.guildFileStructure -- Set the guild defaults
@@ -161,17 +185,15 @@ function core:StartDatabase(clubID)
     ns.gFilterList = ns.guild.filterList -- Set the filter list database
 
     GR.debug = GR.isTesting or ns.pSettings.debugMode -- Set the debug modes
-    if continueConvert then ns.convert:ContinueConvert(whichOne) end
-
-    return continueConvert
+    return wasReset
 end
 function core:LoadTables()
     ns.tblBlackList, ns.antiSpamList = {}, {}
 
-    local blSuccess, tblBL = ns.code:decompressData(ns.guild.blackList or {})
+    local blSuccess, tblBL = ns.code:decompressData(ns.g.blackList or {})
     ns.tblBlackList = blSuccess and tblBL or {}
 
-    local asSuccess, tblAS = ns.code:decompressData(ns.guild.antiSpamList or {})
+    local asSuccess, tblAS = ns.code:decompressData(ns.g.antiSpamList or {})
     ns.tblAntiSpamList = asSuccess and tblAS or {}
 
     --* Load Class/Race and Invalid Zones Table
@@ -282,7 +304,7 @@ function core:StartupGuild(clubID)
     local function createGuildLink(retry)
         retry = retry + 1
 
-        local club = clubID and C_ClubFinder.GetRecruitingClubInfoFromClubID(clubID) or nil
+        local club = clubID and ClubFinderGetCurrentClubListingInfo(clubID) or nil
 
         if club then
             local guildLink = "|cffffd200|HclubFinder:"..club.clubFinderGUID.."|h["..club.name.."]|h|r"
@@ -354,15 +376,6 @@ local function eventsPLAYER_LOGIN()
     GR:UnregisterEvent('PLAYER_LOGIN', eventsPLAYER_LOGIN)
 
     core:CheckIfInGuild(0, function(clubID)
-        local function checkForFGI(retry)
-            retry = retry + 1
-            local isFGILoaded = C_AddOns.IsAddOnLoaded('FastGuildInvite')
-            if isFGILoaded then ns.code:fOut(L['FGI_LOADED'], ns.COLOR_ERROR) return
-            elseif retry > 5 then return
-            elseif retry <= 5 then C_Timer.After(1, function() checkForFGI(retry + 1) end) end
-        end
-        checkForFGI(0)
-
         if clubID then core:StartGuildRecruiter(clubID) end
     end)
 end
